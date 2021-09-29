af08f9b6e50ef8fa1a59b43fc41afbd97f694eeb
Oleh2506 <oleg3322110@gmail.com>
2021-09-29 12:26:07 +0300

    Comments were corrected 2.0

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,6 +1,6 @@
 #variant 2
 
-S1 = float(input('S1 = ')) #assignment of S1, S1 -- area of the circumscribed square
+S1 = float(input('S1 = ')) #input of S1, S1 -- area of the circumscribed square
 S2 = S1 / 2 #calculation of S2, S2 -- area of the inscribed square
 print('S2 = {0}'.format(S2)) #output of the result
 print('S1 / S2 = 2') #output of the result

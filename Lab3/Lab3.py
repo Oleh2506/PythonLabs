88cd96ed27794f5fac6d3b889f9d4118a78e5b03
Oleh2506 <oleg3322110@gmail.com>
2021-10-12 18:25:01 +0300

    Lab3 was added

---
+++ Lab3/Lab3.py
@@ -0,0 +1,43 @@
+# variant 2
+
+# Program to find natural logarithm using mathematical series
+
+import math
+
+varA = float(input('Input variable \"a\" in the range (0;2]: ')) #initializing a variable, the natural logarithm of which will be calculated
+
+# checking the correctness of input data
+# inputDataIsCorrect -- boolean variable that describes the correctness of input data
+if varA >= 0 and varA <= 2: 
+    inputDataIsCorrect = True 
+else:
+    inputDataIsCorrect = False
+
+
+if inputDataIsCorrect:
+    CALCULATION_ACCURACY = 0.000001 # accuracy of calculation
+    currentElem = varA - 1 # current element of the series
+    LnA = 0 # natural log of a
+    i = 0 # counter
+    nextIteration = True # boolean variable for the do-while loop emulating   
+
+    print('\nElements of the series: ')
+
+    # finding the natural logarithm value
+    while nextIteration:
+        print('{1} [{0}]'.format(i, currentElem), end = '; ') # element of the series output
+        LnA += currentElem
+        i += 1
+        currentElem *= ((-1) * (varA - 1) * i) / (i + 1)
+        if abs(currentElem) <= CALCULATION_ACCURACY: # Exit condition
+            nextIteration = False
+
+    # percent error calculation
+    if varA != 1:
+        percentError = abs(LnA - math.log1p(varA - 1)) / math.log1p(varA - 1) * 100;
+    else:
+        percentError = 0
+
+    print('\n\nLn(a): {0}\nPercent error: {1} %\n'.format(LnA, percentError)) # result message output
+else:
+    print('Error: input data aren\'t correct!') # error message output
